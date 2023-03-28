c6514d3654032e52306c89da3c3d37d26e0a7203
FangzhouSu <1773279395@qq.com>
2023-03-28 17:11:00 +0800

    ✨ feat(简历首页): 实现简历首页 可以预览简历主体

--- app/renderer/container/resume/index.tsx
+++ app/renderer/container/resume/index.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import fileAction from "@common/utils/file";
-import { getAppPath } from "@common/utils/appPath";
 import "./index.less";
+// 👇 将组件引入
+import ResumeAction from "./ResumeAction";
+import ResumeContent from "./ResumeContent";
+import ResumeToolbar from "./ResumeToolbar";
 
 function Resume() {
-  // 👇 读取一下当前这个文件内容
-  getAppPath().then((rootPath: string) => {
-    console.log("应用程序的目录路径为: ", rootPath);
-    console.log("文件读取，内容数据为: ");
-    fileAction
-      .read(`${rootPath}app/renderer/container/resume/index.tsx`)
-      .then((data) => {
-        console.log(data);
-      });
-  });
-
-  return <div>我是简历模块</div>;
+  return (
+    <div styleName="container">
+      <div styleName="header">
+        <ResumeAction />
+      </div>
+      <div styleName="content">
+        <ResumeContent />
+      </div>
+      <div styleName="toolbar">
+        <ResumeToolbar />
+      </div>
+    </div>
+  );
 }
 export default Resume;
--- app/renderer/store/resumeModel.ts
+++ app/renderer/store/resumeModel.ts
@@ -26,28 +26,18 @@ const resumeModel: TSRcReduxModel.Props<TSResume.IntactResume> = {
     },
     hobby: '看剧',
     skill:
-      '熟悉 Vue.js，了解数据双向绑定原理、阅读过 NextTick 源码｜熟悉 React，了解并使用 Hooks 特性，阅读过 redux 源码，开发 rc-redux-model 中间件｜阅读过 Antd 部分优秀组件源码，并参考借鉴，开发组内 UI 组件库｜了解 Vscode，开发组内项目辅助工具 vscode-beehive-extension 插件｜了解 Webpack 编译原理，了解 babel 转码原理，编写过 babel 插件｜了解 Electron，了解 Node.js 以及 Git 团队协作开发工具｜了解设计模式，对于特定场景，能采用合适的设计模式进行解决｜了解 MYSQL，了解数据库优化常用方法｜了解基于微信公众号应用开发，采用 Taro 开发微信小程序，具备良好的网络基础知识',
+      '熟悉 Vue.js｜熟悉 React，了解并使用 Hooks 特性｜了解 MYSQL，了解数据库优化常用方法',
     skillList: [
-      '熟悉 Vue.js，了解数据双向绑定原理、阅读过 NextTick 源码',
-      '熟悉 React，了解并使用 Hooks 特性，阅读过 redux 源码，开发 rc-redux-model 中间件',
-      '阅读过 Antd 部分优秀组件源码，并参考借鉴，开发组内 UI 组件库',
-      '了解 Vscode，开发组内项目辅助工具 vscode-beehive-extension 插件',
-      '了解 Webpack 编译原理，了解 babel 转码原理，编写过 babel 插件',
-      '了解 Electron，了解 Node.js 以及 Git 团队协作开发工具',
-      '了解设计模式，对于特定场景，能采用合适的设计模式进行解决',
+      '熟悉 Vue.js，了解数据双向绑定原理',
+      '熟悉 React，了解并使用 Hooks 特性',
       '了解 MYSQL，了解数据库优化常用方法',
-      '了解基于微信公众号应用开发，采用 Taro 开发微信小程序，具备良好的网络基础知识',
     ],
     evaluation:
-      '投身开源，rc-redux-model 库作者，SugarTurboS Club 开源组织负责人| 掘金 lv3 博主，掘金文章 10w+ 阅读量，github blog 300+ star | 具备良好语言表达能力和沟通能力，能快速融入团队，适应新环境|具有代码洁癖，前后端分离，自我学习能力强，对新技术具有钻研精神',
+      '',
     evaluationList: [
-      '投身开源，rc-redux-model 库作者，SugarTurboS Club 开源组织负责人',
-      '掘金 lv3 博主，掘金文章 10w+ 阅读量，github blog 300+ star',
-      '具备良好语言表达能力和沟通能力，能快速融入团队，适应新环境。',
-      '具有代码洁癖，前后端分离，自我学习能力强，对新技术具有钻研精神',
     ],
-    certificate: '广州第一届喝酒大赛参与奖',
-    certificateList: ['广州第一届喝酒大赛参与奖'],
+    certificate: '',
+    certificateList: [],
     schoolExperience: [
       {
         beginTime: '2016.09',
